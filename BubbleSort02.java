b2fc7a29b3819d4e859c04b48cfe36f4c4f8cd0b
Hiru-hub <hirushainduwara828@gmail.com>
2024-10-28 23:45:25 +0530

    Add files via upload

---
+++ BubbleSort02.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class BubbleSort02 {
+
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] < array[j + 1]) {
+                    array[j] = array[j] + array[j + 1];
+                    array[j + 1] = array[j] - array[j + 1];
+                    array[j] = array[j] - array[j + 1];
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[8];
+
+        System.out.println("Enter 8 numbers:");
+        for (int i = 0; i < 8; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        bubbleSort(numbers);
+
+        System.out.println("Sorted numbers in descending order:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        scanner.close();
+    }
+}
